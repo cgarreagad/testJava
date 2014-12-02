159e38056fdfe8cb83077df50e180e6281c0f7c4
cgad070313 <cgad070313@uio11cgad070313.sri.ad>
2014-12-02 17:02:32 -0500

    GO

---
+++ jee7-primefaces-pc/src/main/java/ec/gob/sri/estandar/jee7/beans/Controlador01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ec.gob.sri.estandar.jee7.beans;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * @author cgad070313
+ *
+ */
+@Named
+@RequestScoped
+public class Controlador01 {
+
+}
